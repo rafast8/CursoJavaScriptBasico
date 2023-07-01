e2b445a20a28b3ce6ec5f14ef58e12de2d01844a
rafast8 <rafast8@outlook.es>
2023-07-01 19:44:07 +0200

    Update Ejercicio3.js

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -1,22 +1,32 @@
 // Cadena de texto - Ejercicio
 
+// Una cadena de texto con tu Nombre
 let nombre = "Rafael";
+
+// Una cadena de texto con tu Apellido
 let apellidos = "Toca los";
+
+// Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
 let estudiante = `${nombre} ${apellidos}`;
 console.log(estudiante);
 
+// Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
 let estudianteMayus = estudiante.toUpperCase();
 console.log(estudianteMayus);
 
+// - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
 let estudianteMinus = estudiante.toLowerCase();
 console.log(estudianteMinus);
 
+// - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
 let contarCaracteres = estudiante.length;
 console.log(contarCaracteres);
 
+// - Una variable que contenga la primera letra del Nombre
 let primeraLetraDelNombre = estudiante[0];
 console.log(primeraLetraDelNombre);
 
+// - Otra variable que contenga la última letra del Nombre
 let longitud = nombre.length;
 console.log(longitud)
 let ultimaLetraDelNombre = estudiante[longitud-1];
