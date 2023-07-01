0fd026ba8a341858db4bf3115f6cb69d870d01f6
rafast8 <137616387+rafast8@users.noreply.github.com>
2023-07-01 11:49:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Esta es la puerta de entrada al proyecto hola_mundo")
+
+
+let variableLet = "Hola soy una variable LET";
+console.log(variableLet);
+for (var i = 0; i < 3; i++) {
+     variableLet = "Soy la segunda variable LET";
+}
+console.log(variableLet);
+console.log("Adiós")
